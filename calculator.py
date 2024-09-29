e64f0360ce4a1cd3be6a5b620d07635551b567e9
amithsoddy <anithasoddy@gmail.com>
2024-09-29 18:30:12 +0530

    function

---
+++ calculator.py
@@ -0,0 +1,14 @@
+def add(x,y):
+    return x+y
+def sub(x,y):
+    return x-y
+def mul(x,y):
+    return x*y
+def div(x,y):
+    return x/y
+a=int(input("enter a number"))  
+b=int(input("enter a number")) 
+print(add(a,b))
+print(sub(a,b))
+print(mul(a,b))
+print(div(a,b))
---
+++ intro.py
@@ -0,0 +1,4 @@
+def intro(name):
+    print("hello my name is ", name)
+y=str(input("enter your name"))
+intro(y)
